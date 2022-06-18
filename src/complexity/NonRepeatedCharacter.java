92d3d1bd5d18472dd23e32967d30c8e218321f29
Rodolfo Borges <rodolfobborges@gmail.com>
2022-06-18 15:55:32 +0100

    Implementation for getting the first non repeating char from a given string

---
+++ src/complexity/NonRepeatedCharacter.java
@@ -0,0 +1,33 @@
+package complexity;
+
+import java.util.Arrays;
+
+public class NonRepeatedCharacter {
+
+    /**
+     * given a String this function should return the first non repeated character
+     * Example: if "rodolfo borges" then should return 'd'
+     */
+    public char getFirstNonRepeatedCharacter(String input) {
+        char firstNonRepeatedChar = ' ';
+        char[] characters = input.toCharArray();
+        for (char chari : characters) {
+            int count = 0;
+            for (char charj : characters) {
+                if (chari == charj) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                firstNonRepeatedChar = chari;
+                break;
+            }
+        }
+        return firstNonRepeatedChar;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new NonRepeatedCharacter().getFirstNonRepeatedCharacter("rodolfo borges"));
+        System.out.println(new NonRepeatedCharacter().getFirstNonRepeatedCharacter("dddddddoooooooooooog"));
+    }
+}
